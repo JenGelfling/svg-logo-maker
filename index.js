a727bdb01055a8a598bbb656e6e771d68e77a8c8
Ben Merchant <benmerch@gmail.com>
2024-08-01 17:13:59 -0500

    Finished the classes and functions to create the svg.

--- index.js
+++ index.js
@@ -1,8 +1,283 @@
 
-const Shapes = require('./lib/shapes');
-const shape = new Shapes();
+const inquirer = require("inquirer")
+const fs = require('fs');
+// const { gatherInfo } = require("./lib/shapes");
 
-shape.run();
+// async function start(){
+  
+//     const data = await inquirer.prompt(gatherInfo);
+//     const result = 
 
 
 
+
+
+
+
+
+// const { default: inquirer } = require("inquirer");
+
+const shapeOptions = ["Circle", "Square", "Triangle"]
+
+const colorOptions = {
+    "aliceblue":"#f0f8ff",
+    "antiquewhite":"#faebd7",
+    "aqua":"#00ffff",
+    "aquamarine":"#7fffd4",
+    "azure":"#f0ffff",
+    "beige":"#f5f5dc",
+    "bisque":"#ffe4c4",
+    "black":"#000000",
+    "blanchedalmond":"#ffebcd",
+    "blue":"#0000ff",
+    "blueviolet":"#8a2be2",
+    "brown":"#a52a2a",
+    "burlywood":"#deb887",
+    "cadetblue":"#5f9ea0",
+    "chartreuse":"#7fff00",
+    "chocolate":"#d2691e",
+    "coral":"#ff7f50",
+    "cornflowerblue":"#6495ed",
+    "cornsilk":"#fff8dc",
+    "crimson":"#dc143c",
+    "cyan":"#00ffff",
+    "darkblue":"#00008b",
+    "darkcyan":"#008b8b",
+    "darkgoldenrod":"#b8860b",
+    "darkgray":"#a9a9a9",
+    "darkgreen":"#006400",
+    "darkkhaki":"#bdb76b",
+    "darkmagenta":"#8b008b",
+    "darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00",
+    "darkorchid":"#9932cc",
+    "darkred":"#8b0000",
+    "darksalmon":"#e9967a",
+    "darkseagreen":"#8fbc8f",
+    "darkslateblue":"#483d8b",
+    "darkslategray":"#2f4f4f",
+    "darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3",
+    "deeppink":"#ff1493",
+    "deepskyblue":"#00bfff",
+    "dimgray":"#696969",
+    "dodgerblue":"#1e90ff",
+    "firebrick":"#b22222",
+    "floralwhite":"#fffaf0",
+    "forestgreen":"#228b22",
+    "fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc",
+    "ghostwhite":"#f8f8ff",
+    "gold":"#ffd700",
+    "goldenrod":"#daa520",
+    "gray":"#808080",
+    "green":"#008000",
+    "greenyellow":"#adff2f",
+    "honeydew":"#f0fff0",
+    "hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c",
+    "indigo":"#4b0082",
+    "ivory":"#fffff0",
+    "khaki":"#f0e68c",
+    "lavender":"#e6e6fa",
+    "lavenderblush":"#fff0f5",
+    "lawngreen":"#7cfc00",
+    "lemonchiffon":"#fffacd",
+    "lightblue":"#add8e6",
+    "lightcoral":"#f08080",
+    "lightcyan":"#e0ffff",
+    "lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3",
+    "lightgreen":"#90ee90",
+    "lightpink":"#ffb6c1",
+    "lightsalmon":"#ffa07a",
+    "lightseagreen":"#20b2aa",
+    "lightskyblue":"#87cefa",
+    "lightslategray":"#778899",
+    "lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0",
+    "lime":"#00ff00",
+    "limegreen":"#32cd32",
+    "linen":"#faf0e6",
+    "magenta":"#ff00ff",
+    "maroon":"#800000",
+    "mediumaquamarine":"#66cdaa",
+    "mediumblue":"#0000cd",
+    "mediumorchid":"#ba55d3",
+    "mediumpurple":"#9370d8",
+    "mediumseagreen":"#3cb371",
+    "mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a",
+    "mediumturquoise":"#48d1cc",
+    "mediumvioletred":"#c71585",
+    "midnightblue":"#191970",
+    "mintcream":"#f5fffa",
+    "mistyrose":"#ffe4e1",
+    "moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead",
+    "navy":"#000080",
+    "oldlace":"#fdf5e6",
+    "olive":"#808000",
+    "olivedrab":"#6b8e23",
+    "orange":"#ffa500",
+    "orangered":"#ff4500",
+    "orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa",
+    "palegreen":"#98fb98",
+    "paleturquoise":"#afeeee",
+    "palevioletred":"#d87093",
+    "papayawhip":"#ffefd5",
+    "peachpuff":"#ffdab9",
+    "peru":"#cd853f",
+    "pink":"#ffc0cb",
+    "plum":"#dda0dd",
+    "powderblue":"#b0e0e6",
+    "purple":"#800080",
+    "rebeccapurple":"#663399",
+    "red":"#ff0000",
+    "rosybrown":"#bc8f8f",
+    "royalblue":"#4169e1",
+    "saddlebrown":"#8b4513",
+    "salmon":"#fa8072",
+    "sandybrown":"#f4a460",
+    "seagreen":"#2e8b57",
+    "seashell":"#fff5ee",
+    "sienna":"#a0522d",
+    "silver":"#c0c0c0",
+    "skyblue":"#87ceeb",
+    "slateblue":"#6a5acd",
+    "slategray":"#708090",
+    "snow":"#fffafa",
+    "springgreen":"#00ff7f",
+    "steelblue":"#4682b4",
+    "tan":"#d2b48c",
+    "teal":"#008080",
+    "thistle":"#d8bfd8",
+    "tomato":"#ff6347",
+    "turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3",
+    "white":"#ffffff",
+    "whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00",
+    "yellowgreen":"#9acd32"
+};
+
+function isValidColor(input) {
+    return Object.values(colorOptions).includes(input) || Object.keys(colorOptions).includes(input);
+  }
+
+
+function gatherInfo(){
+        return [
+            {
+                type: "list",
+                message: "Select the shape of your logo.",
+                name: "shape",
+                choices: shapeOptions
+              },
+              {
+                type: "input",
+                message: "What color do you want the logo?",
+                name: "shapeColor",
+                choices: colorOptions,
+                validate: input => isValidColor(input) || 'Please enter a valid color name or hex value.'
+              },
+              {
+                type: "input",
+                message: "Enter up to three characters for your logo.",
+                name: "text",
+                validate: input => input.length <= 3 || 'Text must be up to three characters long.'
+              },
+              {
+                type: "input",
+                message: "What color do you want the text?",
+                name: "textColor",
+                choices: colorOptions,
+                validate: input => isValidColor(input) || 'Please enter a valid color name or hex value.'
+              }
+        ]
+};
+
+
+class Shape {
+    constructor(shapeColor, text, textColor){
+        this.shapeColor = shapeColor;
+        this.textColor = textColor;
+        this.text = text;
+    }
+}
+
+
+class Circle extends Shape {
+        render(){
+            return `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
+
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+            </svg>
+            `
+    }
+}
+
+
+class Triangle extends Shape {
+    render(){
+        return `
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        <polygon points="50 15, 100 100, 0 100" fill="${this.shapeColor}" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        </svg>
+        `
+    }
+}
+
+
+class Square extends Shape {
+    render(){
+        return `
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        <rect x="60" y="10" rx="10" ry="10" fill="${this.shapeColor}" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        </svg>
+        `
+    }
+}
+
+
+
+async function createSvg(){
+    try {
+        const response = await inquirer.prompt(gatherInfo());   
+        let shape;
+        if(response.shape === "Circle"){
+            shape = new Circle(response.shapeColor, response.text, response.textColor)
+        }
+        else if(response.shape === "Square"){
+            shape = new Square(response.shapeColor, response.text, response.textColor) 
+        }
+        else if(response.shape === "Triangle"){
+            shape = new Triangle(response.shapeColor, response.text, response.textColor) 
+        }
+        if (shape){
+            const newSvg = shape.render();
+            fs.writeFile("logo.svg", newSvg, function(err){
+                if(err) console.log(err)
+            })
+        }
+    } catch (error) {
+    console.error('Error occurred:', error)
+    }
+}
+
+
+createSvg();
+
+
+// module.exports = { gatherInfo }
